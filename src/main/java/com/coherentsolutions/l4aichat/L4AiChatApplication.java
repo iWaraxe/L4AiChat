65d63b50f69bbd5a9ec6dc8b1523e15cc803e123
iWaraxe <igor.waraxe@gmail.com>
2025-04-21 11:47:59 +0200

    Initial Commit

---
+++ src/main/java/com/coherentsolutions/l4aichat/L4AiChatApplication.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.l4aichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L4AiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L4AiChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coherentsolutions/l4aichat/L4AiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.l4aichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L4AiChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
